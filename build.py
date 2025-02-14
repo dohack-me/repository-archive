46fbf89a2bcfdd385315f995996485a4a93e02e1
DiffuseHyperion <95083897+DiffuseHyperion@users.noreply.github.com>
2025-02-14 18:20:37 +0800

    standardize file format of Hack@AC 2024 web challenges and add missing expose instructions to dockerfiles

--- build.py
+++ build.py
@@ -1,9 +1,9 @@
 import os
 import subprocess
 
-def find_dockerfiles():
+def find_dockerfiles(root_dir):
     dockerfiles = []
-    for root, dirs, files in os.walk("."):
+    for root, dirs, files in os.walk(root_dir):
         if "Dockerfile" in files:
             dockerfiles.append(os.path.join(root, "Dockerfile"))
     return dockerfiles
@@ -14,9 +14,9 @@ def clean(value):
 
 to_build = {}
 to_delete = []
-for dockerfile in find_dockerfiles():
+for dockerfile in find_dockerfiles(os.path.join(".", "Hack@AC_2024")):
     repository, category, name, folder = (clean(value) for value in dockerfile.split(os.sep)[1:5])
-    tag = f"registry.dohack.me/{repository}/{category}/{name}:latest"
+    tag = f"dohackme.azurecr.io/{repository}/{category}/{name}:latest"
     if folder == "src":
         if tag in to_build.keys():
             to_delete.append(tag) # Repeat tags probably mean the challenge requires a stack, and should be skipped
@@ -29,7 +29,7 @@ for tag in to_delete:
 
 for tag, location in to_build.items():
     print(f"Building {tag}")
-    result = subprocess.run(["docker", "build", "-t", tag, "-f", location, os.path.dirname(location)], shell=True,
+    result = subprocess.run(["docker", "build", "-t", tag, "-f", location, os.path.dirname(location)],
                             capture_output=True, text=True)
     if result.returncode == 0:
         print(f"{tag} built successfully")
